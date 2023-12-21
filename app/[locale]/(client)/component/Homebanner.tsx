824169242827d2ca8db82d3628cd17fc6d089a80
v2kid <vgiakhang@gmail.com>
2023-12-21 13:53:36 +0700

    'a'

--- app/[locale]/(client)/component/Homebanner.tsx
+++ app/[locale]/(client)/component/Homebanner.tsx
@@ -23,7 +23,9 @@ export default function Homebanner(){
             <div className="flex-shrink px-4 max-w-full w-full sm:w-1/2 lg:w-1/3 lg:px-6 wow fadeInUp" data-wow-duration="1s" >
                 <div className="py-8 px-12 mb-12 bg-gray-50 bg-opacity-0 border-b border-gray-100 transform transition duration-300 ease-in-out hover:-translate-y-2">
                     <div className="inline-block text-slate-300 mb-4">
+                    <a  target="_blank" href="https://back-end-next14.onrender.com/api">
                     <Image className=" z-10 h-[60px]" src="/nest-js-icon.png" height={100} width={100} alt="icon"/>
+                    </a>
                     </div>
                     <h3 className="text-lg leading-normal mb-2 font-semibold text-slate-300">Nest JS</h3>
                     <p className="text-gray-300">Back-End Nestjs</p>
